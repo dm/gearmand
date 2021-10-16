32385ebd2aab4bcd3d438c6d051c9ecf2f9f3761
Ed Sabol <edwardjsabol@gmail.com>
2021-10-15 23:47:59 -0400

    Tweak scripts/travis-linux.sh to fix clang-9 build

--- scripts/travis-linux.sh
+++ scripts/travis-linux.sh
@@ -1,3 +1,6 @@
 sudo apt-get update -qq
 COMPILER_PACKAGE=$CXX
+if echo "$COMPILER_PACKAGE" | grep -q '^clang++-'; then
+    COMPILER_PACKAGE=$CC
+fi
 sudo apt-get install -y libboost-all-dev gperf libevent-dev uuid-dev python-sphinx libhiredis-dev $COMPILER_PACKAGE
