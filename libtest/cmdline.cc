4b46bb21b2dd6bc6a42fcf191f363d6386a4172f
Ed Sabol <esabol@users.noreply.github.com>
2021-11-01 13:14:20 -0400

    Issue #321: Improve compatibility with older versions of Alpine (#328)
    * Issue #321: Improve compatibility with older versions of Alpine

--- libtest/cmdline.cc
+++ libtest/cmdline.cc
@@ -204,8 +204,8 @@ Application::error_t Application::run(const char *args[])
 
   fatal_assert(posix_spawnattr_setsigmask(&spawnattr, &mask) == 0);
 
-#if defined(POSIX_SPAWN_USEVFORK) || defined(__linux__)
-  // Use USEVFORK on linux
+#if defined(POSIX_SPAWN_USEVFORK) || defined(__GLIBC__)
+  // Use USEVFORK where appropriate
   flags |= POSIX_SPAWN_USEVFORK;
 #endif
 
